5f67231c11e55585015ee4c1caa243c27bb82cb3
paskuvan <majopaskuvan@gmail.com>
2019-04-08 18:23:19 -0300

    fourth commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  post 'purchases/create'
   resources :products
-  get 'home/index'
   devise_for :users
-	root to: "home#index"
+  resources :users, only: [:index, :show]
+  get 'home/index'
+  get 'home/purchases'
+  get 'home/sales'
+
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
