9bf93c418faeb84b31539095dbe590e492c9a235
paskuvan <majopaskuvan@gmail.com>
2019-04-08 21:22:28 -0300

    sixty commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_010454) do
+ActiveRecord::Schema.define(version: 2019_04_09_001705) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_04_04_010454) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
